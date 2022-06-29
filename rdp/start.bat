71225ee6bb4adca245b4fa849811891161adbc77
server <94029661+avcswap@users.noreply.github.com>
2022-06-29 17:33:31 +0700

    Update start.bat

--- rdp/start.bat
+++ rdp/start.bat
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: administrator
+echo Username: runneradmin
 echo Password: Arimonox@2020
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
