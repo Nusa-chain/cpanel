d3f6800d4583b8fb03fe5632341ac6c2fb7d7323
Block_dev <94029661+Nusa-chain@users.noreply.github.com>
2022-07-15 15:25:29 +0700

    Create start.bat

---
+++ host/start.bat
@@ -0,0 +1,17 @@
+@echo off
+net config server /srvcomment:"Windows Server 2020 by Arimonox" > out.txt 2>&1
+net user administrator Arimonox@2020 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo Success!
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Node runing
+echo You can login now.
+ping -n 10 127.0.0.1 >nul
