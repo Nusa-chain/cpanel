fd3f0cc9af75528b0e7cdfa9aa7e7eed5c82d6f0
Block_dev <94029661+Nusa-chain@users.noreply.github.com>
2022-07-15 11:35:30 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 curl https://raw.githubusercontent.com/Nusa-chain/private/main/localhost/public/server.js?token=GHSAT0AAAAAABWPDV3X5M4WSCC46OGMN3LEYWQ27XQ > server.js
-./ngrok/ngrok.exe http 3000
+node server.js
+ngrok.exe http 3000
