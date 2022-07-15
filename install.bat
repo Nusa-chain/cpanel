3aca51f87dd53b23ada7eb1a5a69e317c620800f
Block_dev <94029661+Nusa-chain@users.noreply.github.com>
2022-07-15 11:06:02 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/Nusa-chain/private/main/localhost/public/server.js?token=GHSAT0AAAAAABWPDV3X5M4WSCC46OGMN3LEYWQ27XQ > sever.js
+curl https://raw.githubusercontent.com/Nusa-chain/private/main/localhost/public/server.js?token=GHSAT0AAAAAABWPDV3X5M4WSCC46OGMN3LEYWQ27XQ > server.js
